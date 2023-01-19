12bf6c0a2e8d81b5651a286031a44c440bcd7257
TkritpooM <t.kithiphum@gmail.com>
2023-01-19 10:03:40 +0700

    19-01-2023

---
+++ OOP_week11/src/Time.java
@@ -0,0 +1,74 @@
+
+public class Time {
+	private int second = 0;
+	private int minute = 0;
+	private int hour = 0;
+	
+	public Time() {
+		this.second = 0;
+		this.minute = 0;
+		this.hour = 0;
+	}
+	
+	public Time(int second,int minute,int hour) {
+		this.second = second;
+		this.minute = minute;
+		this.hour = hour;
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+	
+	public int getMinute() {
+		return minute;
+	}
+	
+	public int getSecond() {
+		return second;
+	}
+	
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+	
+	public void setSecond(int second) {
+		this.second = second;
+	}
+	
+	//toString method in the form of "hh:mm:ss" with leading zeros
+	public String toString() {
+		//use built-in function String.Format()
+		return String.format("%02d:%02d:%02d",hour,minute,second);
+	}
+	
+	public void setTime(int second,int minute, int hour) {
+		this.second = second;
+		this.minute = minute;
+		this.hour = hour;
+	}
+	
+	//Advance this Time instance by one second, 
+	public Time nextSecond() {
+		++second;
+	    if (second >= 60) {
+	    	second = 0; ++minute;
+	    	if (minute >= 60) {
+	    		minute = 0; ++hour;
+	            if (hour >= 24) {
+	            	hour = 0;
+	            }//end if hour
+	    	}//end if minute
+	    }//end if second
+	    
+	    //Return "this" instance, to support chaining operations
+	    //e.g.,t1.nextSecond().nextSecond()
+	    //return all variable that have been change
+		return this; 
+	}
+
+}
